4552700eb00cfa1d1318715410136485112da134
Arwa Mahmoud <arwamah777@gmail.com>
2025-12-18 18:08:07 +0200

    Duplicate alx-graphql-0x02 to alx-graphql-0x03

---
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -0,0 +1,69 @@
+import EpisodeCard from "@/components/common/EpisodeCard"
+import { GET_EPISODES } from "@/graphql/queries"
+import { EpisodeProps } from "@/interfaces"
+import { useQuery } from "@apollo/client/react"
+import { useEffect, useState } from "react"
+
+const Home: React.FC = () => {
+  const [page, setPage] = useState<number>(1)
+  const { loading, error, data, refetch } = useQuery(GET_EPISODES, {
+    variables: {
+      page:page
+    }
+    
+  })
+  useEffect(() => {
+    refetch()
+  },[page, refetch])
+
+  if(loading) return <h1>Loading...</h1>
+  if(error) return <h1>Error</h1>
+
+  const results = data?.episodes.results
+  const info = data?.episodes.info
+  return (
+    <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#A3D5E0] to-[#F4F4F4] text-gray-800">
+      {/* Header */}
+      <header className="bg-[#4CA1AF] text-white py-6 text-center shadow-md">
+        <h1 className="text-4xl font-bold tracking-wide">Rick and Morty Episodes</h1>
+        <p className="mt-2 text-lg italic">Explore the multiverse of adventures!</p>
+      </header>
+
+      {/* Main Content */}
+      <main className="flex-grow p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {results && results.map(({id, name, episode, air_date}: EpisodeProps, key: number) => 
+            <EpisodeCard 
+              key={key}
+              id={id}
+              name={name}
+              episode={episode}
+              air_date={air_date}
+            />
+          )}
+        </div>
+
+        {/* Pagination Buttons */}
+        <div className="flex justify-between mt-6">
+          <button 
+            onClick={() => setPage((pre) => pre>1? pre-1 : pre)}
+            className="bg-[#45B69C] text-white font-semibold py-2 px-6 rounded-lg shadow-lg hover:bg-[#3D9B80] transition duration-200 transform hover:scale-105">
+            Previous
+          </button>
+          <button 
+            onClick={() => setPage((pre) => pre<info.pages? pre+1 : pre)}
+            className="bg-[#45B69C] text-white font-semibold py-2 px-6 rounded-lg shadow-lg hover:bg-[#3D9B80] transition duration-200 transform hover:scale-105">
+            Next
+          </button>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-[#4CA1AF] text-white py-4 text-center shadow-md">
+        <p>&copy; 2024 Rick and Morty Fan Page</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
